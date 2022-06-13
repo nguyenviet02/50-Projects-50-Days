a683bfe6268b57178c1a87e850049a4294f5988c
Viet Nguyen <vietviet2002@gmail.com>
2022-06-13 14:39:44 +0700

    Upload Project of Day4

---
+++ Day4-SearchBar/js/script.js
@@ -0,0 +1,5 @@
+const btn = document.querySelector('.btn');
+const seacrh = document.querySelector('.container');
+btn.onclick = function() {
+  seacrh.classList.toggle('active');
+}
--- Rotating-Navigation/js/script.js
+++
@@ -1,9 +0,0 @@
-const rotate = document.querySelector('.rotate');
-const container = document.querySelector('.container');
-const menu = document.querySelector('.menu');
-
-rotate.onclick = () => {
-  rotate.classList.toggle("active");
-  container.classList.toggle("active");
-  menu.classList.toggle("active");
-}
