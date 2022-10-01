d8bf2b3201f5a761805c8b29211f1699123529af
Viet Nguyen <vietviet2002@gmail.com>
2022-10-01 20:04:35 +0700

    Fix Day 12

--- Day12-FAQ_Collapse/js/script.js
+++ Day12-FAQ_Collapse/js/script.js
@@ -1,7 +1,7 @@
 const question_btn = document.querySelectorAll(".question__btn");
 
 question_btn.forEach((btn) => {
-  btn.onclick = ()=> {
+  btn.onclick = () => {
     btn.closest(".question").classList.toggle("active");
   }
 })
